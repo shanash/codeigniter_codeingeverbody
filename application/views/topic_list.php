a01c666588a5e61b8548bef4aa3a5c83489947fd
Seonghun <shanash511@gmail.com>
2020-10-05 15:28:10 +0900

    메뉴 고정

--- application/views/topic_list.php
+++ application/views/topic_list.php
@@ -1,10 +1,6 @@
-<ol>
-<?php
-$num = 0;
-foreach($list as $data){
-?>
-	<li><a href="/CodeIgniterTest/index.php/topic/get/<?php echo $num;?>"><?php echo $data['nick_name'];?></a></li>
-<?php
-}
-?>
-</ol>
+<ul>
+	<li><a href="/CodeIgniterTest/index.php/topic/get/0">점검</a></li>
+	<li><a href="/CodeIgniterTest/index.php/topic/get/1">이벤트</a></li>
+	<li><a href="/CodeIgniterTest/index.php/topic/get/2">아이템지급</a></li>
+	<li><a href="/CodeIgniterTest/index.php/topic/get/3">아이템회수</a></li>
+</ul>
