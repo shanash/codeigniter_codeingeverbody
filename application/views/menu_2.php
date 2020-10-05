94f06fec98a86badc2eb39c9cf4153c9665204cb
Seonghun <shanash511@gmail.com>
2020-10-05 16:23:59 +0900

    폼 추가

--- application/views/menu_2.php
+++ application/views/menu_2.php
@@ -1,3 +1,20 @@
         <div class="content">
-        토픽 페이지
+                <form method="post" action="$_SERVER[PHP_SELF]">
+                Send Mail<br>
+                user ID: <input type="text" name="uuid"><br>
+                message: <input type="text" name="message"><br>
+                date_deadline: <input type="text" name="date_deadline" value="0"><br>
+                <input type="radio" id="sr_armor_selector" name="item_id" value="600">
+                <label for="sr_armor_selector">궁극 방어구 선택권</label><br>
+                <input type="radio" id="sr_acc_selector" name="item_id" value="601">
+                <label for="sr_acc_selector">궁극 장신구 선택권</label><br>
+                <input type="radio" id="sr_weapon_selector" name="item_id" value="602">
+                <label for="sr_weapon_selector">궁극 무기 선택권</label><br>
+                <input type="radio" id="ssr_treasure_selector" name="item_id" value="700">
+                <label for="ssr_treasure_selector">SSR 보물 선택권</label><br>
+                <input type="radio" id="ur_treasure_selector" name="item_id" value="800">
+                <label for="ur_treasure_selector">UR 보물 선택권</label><br>
+
+                <input type="submit" value="확인">
+                </form>
         </div>
