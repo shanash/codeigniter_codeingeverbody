62564ea9f324a8cfbc1d13727b983ee896525c05
Seonghun <shanash511@gmail.com>
2020-10-06 09:34:09 +0900

    메뉴 스타일 추가

--- application/views/list.php
+++ application/views/list.php
@@ -1,5 +1,5 @@
 		<div class="span2">
-			<ul>
+			<ul class="nav nav=tabs nav-stacked">
 				<li><a href="/CodeIgniterTest/index.php/topic/menu/0">점검</a></li>
 				<li><a href="/CodeIgniterTest/index.php/topic/menu/1">이벤트</a></li>
 				<li><a href="/CodeIgniterTest/index.php/topic/menu/2">아이템지급</a></li>
