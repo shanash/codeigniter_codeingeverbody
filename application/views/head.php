fbf0196b3c41ad78ec5b8070ae66bbdcfc315a99
Seonghun <shanash511@gmail.com>
2020-10-06 09:22:37 +0900

    반응추가

--- application/views/head.php
+++ application/views/head.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="/CodeIgniterTest/static/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link herf="/CodeIgniterTest/static/lib/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
 </head>
 <body>
     <div class="container-fluid">
